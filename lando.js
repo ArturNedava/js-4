2148da63f5e6eda4ab5d52707a39da482c9e516a
ArturNedava <121451856+ArturNedava@users.noreply.github.com>
2023-08-07 22:54:40 +0200

    First commit

---
+++ lando.js
@@ -0,0 +1,9 @@
+let input = document.querySelector("#input");
+let btn = document.querySelector("#btn");
+let inputAtr = input.getAttribute("placeholder");
+btn.textContent = `${inputAtr}`;
+
+
+let img = document.querySelector(".img");
+img.setAttribute("scr","https://st2.depositphotos.com/2927537/7025/i/600/depositphotos_70253417-stock-photo-funny-monkey-with-a-red.jpg");
+
